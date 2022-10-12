41bd201d1438a149bc7e163c86252455bc1bf4fe
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:23:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Webiste Name: [Dev To](https://dev.to/)
+
+// ### Tasks
+
+//         Target the Top description div and change the DEV Community to <Your_Name> and description to your passion
+
+
+// Write your code here:
+
+
+document.querySelector('.side-bar .crayons-card .crayons-subtitle-2');
+<h2 class="crayons-subtitle-2 lh-tight mb-4">…</h2>
+
+document.querySelector('.side-bar .crayons-card .crayons-subtitle-2').innerHTML = 'Yash kaskar';
+'Yash kaskar'
+
+document.querySelector('.side-bar .crayons-card .color-base-70');
+<p class="color-base-70 mb-4">​…​</p>​
+
+document.querySelector('.side-bar .crayons-card .color-base-70').innerText = 'I love to write my own codes';
+'I love to write my own codes'
